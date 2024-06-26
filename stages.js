5d1bb3ffac0ef3e3bdb0824b4504dce4791cc707
pwrignall <pwrignall@users.noreply.github.com>
2024-06-26 17:12:51 +0100

    Change GPX source and use different ETA method

--- stages.js
+++ stages.js
@@ -3,81 +3,81 @@ const stages_data = {
     name: "\u00c9tape 1",
     date: "2024-06-29T12:00:00+0200",
     location: "Florence to Rimini (Italy)",
-    distance: 206.01340297273117,
-    up: 3450.0,
-    down: -3509.0,
+    distance: 206.4531278796486,
+    up: 3657.2400000000007,
+    down: -3729.089999999997,
     type: "Hilly",
   },
   "stage-02": {
     name: "\u00c9tape 2",
     date: "2024-06-30T12:15:00+0200",
     location: "Cesenatico to Bologna (Italy)",
-    distance: 200.55079964534175,
-    up: 1617.0,
-    down: -1555.0,
+    distance: 198.63389770931815,
+    up: 1865.5299999999988,
+    down: -1812.619999999999,
     type: "Hilly",
   },
   "stage-03": {
     name: "\u00c9tape 3",
     date: "2024-07-01T11:15:00+0200",
     location: "Piacenza to Turin (Italy)",
-    distance: 227.54471105256673,
-    up: 1005.0,
-    down: -829.0,
+    distance: 230.11486307570704,
+    up: 1526.039999999999,
+    down: -1338.0400000000004,
     type: "Flat",
   },
   "stage-04": {
     name: "\u00c9tape 4",
     date: "2024-07-02T13:05:00+0200",
     location: "Pinerolo (Italy) to Valloire",
-    distance: 137.9920968012896,
-    up: 3672.0,
-    down: -2676.0,
+    distance: 139.36362934799382,
+    up: 3799.1700000000033,
+    down: -2798.649999999995,
     type: "Mountain",
   },
   "stage-05": {
     name: "\u00c9tape 5",
     date: "2024-07-03T13:20:00+0200",
     location: "Saint-Jean-de-Maurienne to Saint-Vulbas",
-    distance: 177.48187273186306,
-    up: 884.01,
-    down: -1173.01,
+    distance: 177.23890130566122,
+    up: 1022.0900000000004,
+    down: -1308.7199999999991,
     type: "Flat",
   },
   "stage-06": {
     name: "\u00c9tape 6",
     date: "2024-07-04T13:35:00+0200",
     location: "M\u00e2con to Dijon",
-    distance: 163.61446298246642,
-    up: 590.0,
-    down: -586.0,
+    distance: 163.15172757836964,
+    up: 949.1600000000004,
+    down: -946.2500000000011,
     type: "Flat",
   },
   "stage-07": {
     name: "\u00c9tape 7",
     date: "2024-07-05T13:05:00+0200",
     location: "Nuits-Saint-Georges to Gevrey-Chambertin",
-    distance: 25.24462230564409,
-    up: 250.0,
-    down: -228.0,
+    distance: 25.22746255288768,
+    up: 296.8900000000001,
+    down: -274.7100000000001,
     type: "Individual Time Trial",
   },
   "stage-08": {
     name: "\u00c9tape 8",
     date: "2024-07-06T13:05:00+0200",
     location: "Semur-en-Auxois to Colombey-les-Deux-\u00c9glises",
-    distance: 176.62553505609097,
-    up: 2020.0,
-    down: -2004.0,
+    distance: 183.29249930965597,
+    up: 2308.190000000002,
+    down: -2314.4200000000014,
     type: "Flat",
   },
   "stage-09": {
     name: "\u00c9tape 9",
     date: "2024-07-07T13:15:00+0200",
     location: "Troyes to Troyes",
-    distance: 200.5808377033896,
-    up: 1617.0,
-    down: -1621.0,
+    distance: 198.70553931582816,
+    up: 2017.0799999999992,
+    down: -2023.0599999999977,
     type: "Hilly",
   },
   "rest-01": {
@@ -89,54 +89,54 @@ const stages_data = {
     name: "\u00c9tape 10",
     date: "2024-07-09T13:05:00+0200",
     location: "Orl\u00e9ans to Saint-Amand-Montrond",
-    distance: 185.86518976462602,
-    up: 558.0,
-    down: -510.0,
+    distance: 187.0476374048068,
+    up: 876.1600000000009,
+    down: -813.0300000000004,
     type: "Flat",
   },
   "stage-11": {
     name: "\u00c9tape 11",
     date: "2024-07-10T11:20:00+0200",
     location: "\u00c9vaux-les-Bains to <nobr>Le Lioran</nobr>",
-    distance: 211.6557993069147,
-    up: 3879.0,
-    down: -3140.0,
+    distance: 211.18076369893973,
+    up: 4314.1500000000015,
+    down: -3577.680000000002,
     type: "Mountain",
   },
   "stage-12": {
     name: "\u00c9tape 12",
     date: "2024-07-11T12:35:00+0200",
     location: "Aurillac to Villeneuve-sur-Lot",
-    distance: 203.8678853243621,
-    up: 1931.0,
-    down: -2478.0,
+    distance: 203.74306737220525,
+    up: 2131.2299999999977,
+    down: -2670.6199999999994,
     type: "Flat",
   },
   "stage-13": {
     name: "\u00c9tape 13",
     date: "2024-07-12T13:30:00+0200",
     location: "Agen to Pau",
-    distance: 171.62187709929796,
-    up: 1555.0,
-    down: -1385.0,
+    distance: 165.12150491333014,
+    up: 1896.4000000000015,
+    down: -1759.9299999999998,
     type: "Flat",
   },
   "stage-14": {
     name: "\u00c9tape 14",
     date: "2024-07-13T13:05:00+0200",
     location: "Pau to Saint-Lary-Soulan (<nobr>Pla d'Adet</nobr>)",
-    distance: 151.4534798180659,
-    up: 3858.0,
-    down: -2378.0,
+    distance: 151.7185610103865,
+    up: 3976.1500000000024,
+    down: -2503.24,
     type: "Mountain",
   },
   "stage-15": {
     name: "\u00c9tape 15",
     date: "2024-07-14T11:55:00+0200",
     location: "Loudenvielle to Plateau de Beille",
-    distance: 198.3923755318891,
-    up: 4718.0,
-    down: -3967.0,
+    distance: 197.9937620293742,
+    up: 4796.569999999994,
+    down: -4041.560000000003,
     type: "Mountain",
   },
   "rest-02": {
@@ -148,59 +148,57 @@ const stages_data = {
     name: "\u00c9tape 16",
     date: "2024-07-16T13:05:00+0200",
     location: "Gruissan to N\u00eemes",
-    distance: 189.48116269413816,
-    up: 889.0,
-    down: -856.0,
+    distance: 188.0510271894693,
+    up: 1133.61,
+    down: -1143.3299999999997,
     type: "Flat",
   },
   "stage-17": {
     name: "\u00c9tape 17",
     date: "2024-07-17T12:35:00+0200",
     location: "Saint-Paul-Trois-Ch\u00e2teaux to SuperD\u00e9voluy",
-    distance: 178.02957876988114,
-    up: 2728.0,
-    down: -1312.0,
+    distance: 177.53338979033586,
+    up: 2820.9200000000023,
+    down: -1407.5799999999995,
     type: "Mountain",
   },
   "stage-18": {
     name: "\u00c9tape 18",
     date: "2024-07-18T13:00:00+0200",
     location: "Gap to Barcelonnette",
-    distance: 177.48063302924874,
-    up: 2767.0,
-    down: -2573.0,
+    distance: 179.44922132595534,
+    up: 3105.8099999999995,
+    down: -2818.5199999999986,
     type: "Hilly",
   },
   "stage-19": {
     name: "\u00c9tape 19",
     date: "2024-07-19T12:20:00+0200",
     location: "Embrun to <nobr>Isola 2000</nobr>",
-    distance: 142.3001805244168,
-    up: 4325.0,
-    down: -3222.0,
+    distance: 144.69239044284498,
+    up: 4476.379999999994,
+    down: -3345.26,
     type: "Mountain",
   },
   "stage-20": {
     name: "\u00c9tape 20",
     date: "2024-07-20T13:35:00+0200",
     location: "Nice to Col de la Couillole",
-    distance: 133.06154681133668,
-    up: 4558.46,
-    down: -2938.46,
+    distance: 133.82864504343223,
+    up: 4600.83999999999,
+    down: -2982.619999999999,
     type: "Mountain",
   },
   "stage-21": {
     name: "\u00c9tape 21",
     date: "2024-07-21T14:40:00+0200",
     location: "Monaco to Nice",
-    distance: 35.1576359348603,
-    up: 682.0,
-    down: -675.0,
+    distance: 33.67044537976801,
+    up: 696.4800000000004,
+    down: -694.7300000000004,
     type: "Individual Time Trial",
   },
 };
-
-const eta_conv = 3600000;
 
 Object.keys(stages_data).forEach((stageKey) => {
   const topElems = document.createElement("div");
@@ -221,28 +219,41 @@ Object.keys(stages_data).forEach((stageKey) => {
   eta.className = "eta";
   const stageStart = new Date(stages_data[stageKey].date);
   let etaValue = stageStart.getTime();
-
+  const distance = stages_data[stageKey].distance;
   switch (stages_data[stageKey].type) {
     case "Flat":
-      etaValue += ((stages_data[stageKey].distance * 1.1) / 46) * eta_conv;
+      etaValue +=
+        (913.30045 * distance +
+          -4.75334 * Math.pow(distance, 2) +
+          0.00906 * Math.pow(distance, 3) +
+          -48309.76462) *
+        1000;
       break;
     case "Hilly":
-      etaValue += ((stages_data[stageKey].distance * 1.1) / 42) * eta_conv;
+      etaValue +=
+        (11040799.79915 * distance +
+          -56769.52203 * Math.pow(distance, 2) +
+          97.13565 * Math.pow(distance, 3) +
+          -714464057.54687) *
+        1000;
       break;
     case "Mountain":
-      etaValue += ((stages_data[stageKey].distance * 1.1) / 39) * eta_conv;
+      etaValue +=
+        (659.14709 * distance +
+          -3.93346 * Math.pow(distance, 2) +
+          0.00863 * Math.pow(distance, 3) +
+          -24659.38461) *
+        1000;
       break;
     case "Individual Time Trial":
-      if (stageKey == "stage-21") {
-        etaValue += ((stages_data[stageKey].distance * 7) / 50) * eta_conv;
-      } else {
-        etaValue += ((stages_data[stageKey].distance * 9) / 50) * eta_conv;
-      }
+      etaValue += (184.76882 * distance + 11179) * 1000;
       break;
   }
-  eta.textContent = `${new Date(etaValue)
-    .toLocaleTimeString("en-GB")
-    .slice(0, -3)}`;
+  eta.textContent = `${new Date(etaValue).toLocaleTimeString("en-GB", {
+    hour: "numeric",
+    minute: "numeric",
+    timeZoneName: "short",
+  })}`;
 
   const stageHeader = document.createElement("h2");
   stageHeader.textContent = stages_data[stageKey].name;
